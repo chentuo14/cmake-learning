6a70b7c6d5607e59205c7269308796356510dcc6
chen tuo <ch122633@126.com>
2017-08-13 07:08:12 -0700

    hello world

---
+++ helloCMake/build/CMakeFiles/helloCMake.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/helloCMake.dir/main.cpp.o"
+  "helloCMake.pdb"
+  "helloCMake"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/helloCMake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
